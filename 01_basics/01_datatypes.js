b3cd8ed1708ae4eeecc3fc118880e0ab2d3c45ad
yashjaingithub <140307285+yashjaingithub@users.noreply.github.com>
2024-06-04 12:03:45 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"Use strict"; // treat all js code as newer version
+
+//alert (3+3) // we are using node js not browser
+
+console.log(3+3)
+console.log("Yash");
+let name = "Yash"
+let age = 18
+let isloggedin = false
+
+//number = 2 to power 53
+//bigint = for large numbers. used in large websites like facebook, redit, instagram
+//string = ""
+//boolean = true/false
+//null = stand alone value
+//ndefined = when we have not defined value
+//symbol = unique
+
+/* these were all primitive data types*/
+
+//objects
+
+console.log(typeof "Yash"); //give output as string
+console.log(typeof "age"); //give output as number
+console.log(typeof "undefined"); // give output as undefined
+console.log(typeof "null"); //give output as object
