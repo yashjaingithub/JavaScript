f3c9954ed2915f1daf8f3835879a622b8c0cd8f3
yashjaingithub <140307285+yashjaingithub@users.noreply.github.com>
2024-06-04 12:04:19 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+console.log("yash jain");
+const accountID = "12345"
+let accountemail = "yash.yash.com"
+accountpassword = "12211221"
+let accountcity = "mumbai"
+
+//prefer not to use var because of issue in block scope and functional sccope//
+
+console.table([accountID, accountemail, accountpassword, accountcity])
