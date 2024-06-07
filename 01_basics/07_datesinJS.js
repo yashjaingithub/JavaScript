cd49bf81ff06875f0c2e3c04e6c0c33270104062
yashjaingithub <140307285+yashjaingithub@users.noreply.github.com>
2024-06-07 10:30:22 +0000

    dates in JS

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,17 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
