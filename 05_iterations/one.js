5510ea72ddaca373b7d4eceaf5d0035dc39a1ae6
yashjaingithub <140307285+yashjaingithub@users.noreply.github.com>
2024-06-11 10:22:50 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,16 @@
+for (let i  = 0; i  <= 10; i ++) {
+    const element = i ;
+    if(element==5) console.log("5 is best number");
+    console.log(element);
+}
+
+
+
+let myArray = ["flash", "batman", "superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+
+}
+console.log(myArray.length);
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0
+while(index<=10){
+    //console.log(`Value of Index is : ${index}`);
+    index = index +2
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0
+while(arr<myArray.length){
+    //console.log(`my fav super heroes are : ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+
+let score = 1;  // Changed from const to let and initialized to a value less than or equal to 10
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
+
